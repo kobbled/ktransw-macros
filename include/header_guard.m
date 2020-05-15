29c9f76c13b9352aeed67406930133df9c329988
Matt Dewar <mattpdewar@gmail.com>
2020-05-15 01:29:03 -0600

    private guard call for objects

--- include/header_guard.m
+++ include/header_guard.m
@@ -3,6 +3,11 @@
 %mode push
 %mode quote "$"
 %define header_guard ASIS(class_name)_h
+%define header_guard_private ASIS(class_name)_private_h
+
 %defeval header_if $%ifndef header_guard$ 
 %defeval header_def $%define header_guard$
+
+%defeval header_private_if $%ifndef header_guard_private$ 
+%defeval header_private_def $%define header_guard_private$
 %mode pop
