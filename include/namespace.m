11ced057902fcdc8b8f79467cfe28201665e0b96
Matt Dewar <mattpdewar@gmail.com>
2020-08-12 23:14:12 -0600

    refactor GPP methods for instantiating class functions
    -only need to use 'declare_member' before function declaration to transform both `classname__functionname`, and `functionname` into `classname_abreviatedfunctionname`, inside and outside object.
    - declare_function should be used for namespaces.

--- include/namespace.m
+++ include/namespace.m
@@ -13,6 +13,13 @@
 %mode push
 %mode string QQQ "`" "`" "\\"
 %define getalias(nme,fnc,anme,afnc) SILENT(`%defeval TEMP0 nme` `%defeval TEMP1 fnc` `%defeval TEMP2 anme` `%defeval TEMP3 afnc` `%define alias ASIS(TEMP0)ASIS(__)ASIS(TEMP1)` `%define func ASIS(TEMP2)ASIS(_)ASIS(TEMP3)`)
+%define getobject(anme,afnc) SILENT(`%defeval TEMP21 anme` `%defeval TEMP22 afnc` `%define objt ASIS(TEMP21)ASIS(_)ASIS(TEMP22)`)
 %mode pop
 
-%define declare(nme,fnc,anme,afnc) SILENT(%mode push)SILENT(%mode nostring "`")SILENT(getalias(nme,fnc,anme,afnc))ASIS(%defeval TEMP4 `%define alias func`)SILENT(%mode pop)TEMP4
+%define declare_function(nme,fnc,anme,afnc) SILENT(%mode push)SILENT(%mode nostring "`")SILENT(getalias(nme,fnc,anme,afnc))ASIS(%defeval TEMP4 `%define alias func`)SILENT(%mode pop)TEMP4
+
+%mode push
+%mode string QQQ "$" "$" "\\"
+%define declare_member(nme,fnc,anme,afnc) SILENT(%mode push)SILENT(%mode nostring "`")SILENT(getobject(anme,afnc))SILENT(getalias(nme,fnc,anme,afnc))ASIS(%defeval TEMP4 `%define alias func`)ASIS(%defeval TEMP5 `%define fnc objt`)SILENT(%mode pop)$TEMP4
+TEMP5$
+%mode pop
